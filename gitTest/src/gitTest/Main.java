a1aed9292b74133be832037981841bc6c8537588
kimkyeongminimini <kkm961220@gmail.com>
2021-06-23 09:53:40 +0900

    안녕

--- gitTest/src/gitTest/Main.java
+++ gitTest/src/gitTest/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello wolrd!");
+		System.out.println("Hello World!");
+
 	}
 
 }
